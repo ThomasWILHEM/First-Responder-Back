fd46a37cb3eaed11d1a44735b1afcc5458cb157b
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-09-15 10:53:51 +0200

    Buf fixed

--- app/tests/routers/test_router_building_type.py
+++ app/tests/routers/test_router_building_type.py
@@ -10,8 +10,11 @@ def test_get_all_building_type():
     response = client.get("/buildings-types/")
 
     assert response.status_code == 200
-    assert response.json() == [
-        {"id": 1, "name": "Poste de police"},
-        {"id": 2, "name": "Centre hospitalier"},
-        {"id": 3, "name": "Caserne de pompier"},
-    ]
+    assert (response.json() == {
+        "results":
+            [
+                {"id": 1, "name": "Poste de police"},
+                {"id": 2, "name": "Centre hospitalier"},
+                {"id": 3, "name": "Caserne de pompier"},
+            ]
+    })
