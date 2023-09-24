982958810b5b2d2ca17b7c84a6fd4cc3def844f8
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-09-24 15:50:27 +0200

    Tests for building types modified

--- app/tests/routers/test_router_building_type.py
+++ app/tests/routers/test_router_building_type.py
@@ -2,6 +2,16 @@ import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 
+all_types = {
+    "results":
+        [
+            {"id": 1, "name": "Poste de police"},
+            {"id": 2, "name": "Centre hospitalier"},
+            {"id": 3, "name": "Caserne de pompier"},
+        ]
+}
+
+type_4_response = {"id": 4, "name": "test"}
 
 client = TestClient(app)
 
@@ -10,14 +20,7 @@ def test_get_all_building_type():
     response = client.get("/buildings-types/")
 
     assert response.status_code == 200
-    assert (response.json() == {
-        "results":
-            [
-                {"id": 1, "name": "Poste de police"},
-                {"id": 2, "name": "Centre hospitalier"},
-                {"id": 3, "name": "Caserne de pompier"},
-            ]
-    })
+    assert (response.json() == all_types)
 
 
 def test_create_building_type():
@@ -25,21 +28,21 @@ def test_create_building_type():
     response = client.post("/buildings-types/", json=data)
 
     assert response.status_code == 200
-    assert (response.json() == {"id": 4, "name": "test"})
+    assert (response.json() == type_4_response)
 
 
 def test_get_building_type():
     response = client.get("/buildings-types/4")
 
     assert response.status_code == 200
-    assert (response.json() == {"id": 4, "name": "test"})
+    assert (response.json() == type_4_response)
 
 
 def test_delete_building_type():
     response = client.delete("/buildings-types/4")
 
     assert response.status_code == 200
-    assert (response.json() == {"id": 4, "name": "test"})
+    assert (response.json() == type_4_response)
 
     response = client.get("/buildings-types/4")
     assert response.status_code == 404
@@ -47,13 +50,7 @@ def test_delete_building_type():
     response = client.get("/buildings-types/")
 
     assert response.status_code == 200
-    assert (response.json() == {
-        "results":
-            [
-                {"id": 1, "name": "Poste de police"},
-                {"id": 2, "name": "Centre hospitalier"},
-                {"id": 3, "name": "Caserne de pompier"},
-            ]
-    })
+    assert (response.json() == all_types)
+
 
 
