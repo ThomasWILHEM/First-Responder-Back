aa85e372f857a40b9bf622a2a809677fa6c18fac
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-09-27 22:48:06 +0200

    Unit tests for vehicle routers finished

--- app/crud/crud_vehicle.py
+++ app/crud/crud_vehicle.py
@@ -47,14 +47,13 @@ def add_vehicle_to_building(db: Session, vehicle_id: int, building_id: int):
 
 def send_to_call(db: Session, call_id: int, vehicle_id: int):
     call_instance = crud_call.get_call(db, call_id)
-    vehicle_instance = crud_vehicle.get_vehicle(db, vehicle_id)
+    vehicle_instance = get_vehicle(db, vehicle_id)
 
     if not call_instance:
         raise ValueError("Call not found")
     if not vehicle_instance:
         raise ValueError("Vehicle not found")
 
-    call_instance.vehicles.append(vehicle_instance)
     vehicle_instance.call = call_instance
 
     db.commit()
--- app/utils/create_tests_base.sql
+++ app/utils/create_tests_base.sql
@@ -4,12 +4,18 @@ TRUNCATE TABLE building_types;
 TRUNCATE TABLE buildings;
 TRUNCATE TABLE vehicle_types;
 TRUNCATE TABLE vehicles;
+TRUNCATE TABLE scenario_types;
+TRUNCATE TABLE scenarios;
+TRUNCATE TABLE calls;
 
 
 ALTER TABLE building_types AUTO_INCREMENT = 0;
 ALTER TABLE buildings AUTO_INCREMENT = 0;
 ALTER TABLE vehicle_types AUTO_INCREMENT = 0;
 ALTER TABLE vehicles AUTO_INCREMENT = 0;
+ALTER TABLE scenario_types AUTO_INCREMENT = 0;
+ALTER TABLE scenarios AUTO_INCREMENT = 0;
+ALTER TABLE calls AUTO_INCREMENT = 0;
 
 
 INSERT INTO building_types (name) VALUES
@@ -31,5 +37,16 @@ INSERT INTO vehicles (coordinates_latitude, coordinates_longitude, type_id, buil
 (44.582074, -0.032260, 3, 1, null),
 (44.582074, -0.032260, 1, 2, null);
 
+INSERT INTO scenario_types (name) VALUES
+('Police'),
+('Pompier');
+
+INSERT INTO scenarios (name, description, type_id) VALUES
+("Vol de voiture", "Un individu à volé une voiture", 1),
+("Ivresse sur la voie publique", "Un individu est ivre sur la voie publique", 1),
+("Accident de la route", "Un accident à eu lieu sur la voie publique", 2);
+
+INSERT INTO calls (coordinates_latitude, coordinates_longitude, datetime, completion_datetime, scenario_id, mission_status) VALUES
+(44.582476, -0.031490, "2023-09-27 22:28:30", "2023-09-27 22:28:30", 1, "En cours");
 
 SET FOREIGN_KEY_CHECKS = 1;
