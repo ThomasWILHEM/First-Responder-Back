48164ce9cca85c64ba41bbd8d63bad778b745f2a
ThomasWILHEM <thomaswilhempro@outlook.fr>
2023-10-08 18:43:00 +0200

    Views and urls added

--- back/urls.py
+++ back/urls.py
@@ -6,5 +6,22 @@ urlpatterns = [
     path('building-types/<int:pk>', views.BuildingTypeDetails.as_view()),
     path('buildings', views.BuildingList.as_view()),
     path('buildings/<int:pk>', views.BuildingDetails.as_view()),
+    
+    path('vehicle-types', views.VehicleTypeList.as_view()),
+    path('vehicle-types/<int:pk>', views.VehicleTypeDetails.as_view()),
+    path('vehicles', views.VehicleList.as_view()),
+    path('vehicles/<int:pk>', views.VehicleDetails.as_view()),
 
+    path('staff-types', views.StaffTypeList.as_view()),
+    path('staff-types/<int:pk>', views.StaffTypeDetails.as_view()),
+    path('staffs', views.StaffList.as_view()),
+    path('staffs/<int:pk>', views.StaffDetails.as_view()),
+
+    path('scenario-types', views.ScenarioTypeList.as_view()),
+    path('scenario-types/<int:pk>', views.ScenarioTypeDetails.as_view()),
+    path('scenarios', views.ScenarioList.as_view()),
+    path('scenarios/<int:pk>', views.ScenarioDetails.as_view()),
+
+    path('calls', views.CallList.as_view()),
+    path('calls/<int:pk>', views.CallDetails.as_view()),
 ]
--- back/views.py
+++ back/views.py
@@ -22,3 +22,73 @@ class BuildingList(generics.ListCreateAPIView):
 class BuildingDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Building.objects.all()
     serializer_class = BuildingSerializer
+
+
+class VehicleTypeList(generics.ListCreateAPIView):
+    queryset = VehicleType.objects.all()
+    serializer_class = VehicleTypeSerializer
+
+
+class VehicleTypeDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = VehicleType.objects.all()
+    serializer_class = VehicleTypeSerializer
+
+
+class VehicleList(generics.ListCreateAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+
+class VehicleDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+
+class StaffTypeList(generics.ListCreateAPIView):
+    queryset = StaffType.objects.all()
+    serializer_class = StaffTypeSerializer
+
+
+class StaffTypeDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = StaffType.objects.all()
+    serializer_class = StaffTypeSerializer
+
+
+class StaffList(generics.ListCreateAPIView):
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
+
+
+class StaffDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Staff.objects.all()
+    serializer_class = StaffSerializer
+
+
+class ScenarioTypeList(generics.ListCreateAPIView):
+    queryset = ScenarioType.objects.all()
+    serializer_class = ScenarioTypeSerializer
+
+
+class ScenarioTypeDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ScenarioType.objects.all()
+    serializer_class = ScenarioTypeSerializer
+
+
+class ScenarioList(generics.ListCreateAPIView):
+    queryset = Scenario.objects.all()
+    serializer_class = ScenarioSerializer
+
+
+class ScenarioDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Scenario.objects.all()
+    serializer_class = ScenarioSerializer
+
+
+class CallList(generics.ListCreateAPIView):
+    queryset = Call.objects.all()
+    serializer_class = CallSerializer
+
+
+class CallDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Call.objects.all()
+    serializer_class = CallSerializer
